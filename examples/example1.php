7a11824131b1741e7111c5d0c3860d45cba0f675
SPS-Webmaster <webmaster@sourceportservices.com>
2023-03-27 23:10:15 -0600

    Added example code files to the directory.

---
+++ examples/example1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Testing some PHP</title>
+</head>
+<body>
+<h1>Testing some PHP Code Below!</h1>
+<?php
+echo "Look Mom, I am programming with PHP!";
+?>
+</body>
+</html>
---
+++ examples/example2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Testing some PHP</title>
+</head>
+<body>
+<h1>Testing some PHP Code Below!</h1>
+<?php
+// This is a single-line comment
+echo "Look Mom, I am programming with PHP!";
+/* This is a multi-line comment
+   This line will also not be processed by PHP and is still commented until you use the asterisk and forward slash symbols to end the block comment
+*/
+?>
+</body>
+</html>
